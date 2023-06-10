52338ce46c6c0664d21dd1184e95c430e09b78af
scc <zxc25077667@gmail.com>
2023-06-11 02:48:27 +0800

    feat(ci): improve linting script
    The script now excludes `.venv` directories when searching for Python files to lint. Additionally, it uses `xargs` to pass the file list to `pylint`, improving performance. The regular expression used to extract the score from `pylint.out` has been revised from Perl into extended regex.

--- ci/check_lint/unix.sh
+++ ci/check_lint/unix.sh
@@ -4,18 +4,18 @@ set -e
 # Pylint threshold
 LINT_THRESHOLD=9.5
 
-# find all python files
-python_files=$(find . -name "*.py" | tr '\n' ' ') 
+# find all python files, but not .venv files
+python_files=$(find . -type f -name "*.py" -not -path "./.venv/*")
 
 # run pylint
-poetry run python -m pylint --output-format=parseable --reports=no $python_files > pylint.out || true
+echo "$python_files" | xargs poetry run python -m pylint --output-format=parseable --reports=no >pylint.out
 
 # check pylint score
-# Due to the macOS doesn't support -P flag, we need to use grep to get the score
-# We revise the regex from Perl into extended regex
-score=$(grep -oE '(?<=^Your\ code\ has\ been\ rated\ at\ )[0-9\.]+' pylint.out)
+# Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00) ...
+# Sed command to extract the score
+score=$(sed -n 's/^Your code has been rated at \([0-9.]*\)\/.*/\1/p' pylint.out)
 
-if (( $(echo "$score < $LINT_THRESHOLD" | bc -l) )); then
+if (($(echo "$score < $LINT_THRESHOLD" | bc -l))); then
     echo "Pylint score is $score, which is below the threshold of $LINT_THRESHOLD"
     exit 1
 else
