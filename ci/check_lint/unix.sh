caf21fbe7de62cf7aa606b8093dbb22c77fdde9e
scc <scc@teamt5.org>
2024-04-29 15:59:56 +0800

    chore: Update pylint and astroid versions
    Update pylint version to 2.17.7 and astroid version to 2.15.8 for linting.
    Change the behavior of script to continue execution even if a command fails instead of exiting immediately.
    Capture both stdout and stderr when running pylint for better error handling in the script.

--- ci/check_lint/unix.sh
+++ ci/check_lint/unix.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-set -e
+set +e # Continue execution even if a command fails
+
 # Pylint threshold
 LINT_THRESHOLD=9.5
 
 # find all python files, but not .venv files
 python_files=$(find . -type f -name "*.py" -not -path "./.venv/*")
 
-# run pylint
-echo "$python_files" | xargs poetry run python -m pylint --output-format=parseable --reports=no >pylint.out
+# run pylint and capture both stdout and stderr
+echo "$python_files" | xargs poetry run python -m pylint --output-format=parseable --reports=no >pylint.out 2>&1
 
-# check pylint score
+# Output pylint results
+cat pylint.out
+
+# Check pylint score
 # Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00) ...
 # Sed command to extract the score
 score=$(sed -n 's/^Your code has been rated at \([0-9.]*\)\/.*/\1/p' pylint.out)
 
+# Compare score with threshold using bc for floating point comparison
 if (($(echo "$score < $LINT_THRESHOLD" | bc -l))); then
     echo "Pylint score is $score, which is below the threshold of $LINT_THRESHOLD"
-    exit 1
+    exit 1 # Uncomment this line to fail the build if the score is below the threshold
 else
     echo "Pylint score is $score, which is above the threshold of $LINT_THRESHOLD"
 fi
